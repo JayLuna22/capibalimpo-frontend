7c0db78c7fa8cfa9e4e6d67605be1cc1e6437022
Lucas Souza <lucasamarosouza1@gmail.com>
2022-08-15 19:46:44 -0300

    adicionar footer

---
+++ APP/frontend/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () =>{
+    return (
+        <div>
+            <footer>
+                Projeto 
+            </footer>
+        </div>
+
+
+    )
+}
+
+export default Footer;
